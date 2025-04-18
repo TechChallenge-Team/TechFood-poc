51541941703ad108018c730b100608fc383fec64
Elias Soares <eliassoaressouza@gmail.com>
2025-04-18 14:31:52 -0300

    docker: (ajustes docker atualização do banco e tabelas funcionando)

--- src/TechFood.Api/Program.cs
+++ src/TechFood.Api/Program.cs
@@ -30,30 +30,44 @@ var builder = WebApplication.CreateBuilder(args);
 
     builder.Services.AddEndpointsApiExplorer();
 
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Title = "TechFood API V1",
+            Version = "v1",
+            Description = "TechFood API V1",
+        });
+    });
 
     builder.Services.AddApplication();
     builder.Services.AddInfraData();
 }
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger(options =>
     {
-        app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.UseRouting();
-
-    app.UseCors();
-
-    app.UseAuthorization();
-
-    app.MapControllers();
-
-    app.Run();
+        options.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi2_0;
+    });
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TechFood API V1");
+    });
 }
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
