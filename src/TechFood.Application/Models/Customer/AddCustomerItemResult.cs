525b8d9c338bd19a4aefca03b1ca7365f915c913
Elias Soares <eliassoaressouza@gmail.com>
2025-04-24 15:47:41 -0300

    feat: (Criação do CustomerController e camadas da aplicação até o baco)

---
+++ src/TechFood.Application/Models/Customer/AddCustomerItemResult.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace TechFood.Application.Models.Customer
+{
+    public class AddCustomerItemResult
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/TechFood.Application/Models/Customer/CreateCustomerRequest.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TechFood.Application.Models.Customer
+{
+    public class CreateCustomerRequest
+    {
+        [Required]
+        public string CPF { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        
+    }
+}
