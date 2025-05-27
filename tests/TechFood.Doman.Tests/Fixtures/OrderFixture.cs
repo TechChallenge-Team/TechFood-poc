34ac1295a4f8a2f340a41b98f98de68241a7a1c1
leandro oliveira cervantes <leandro_10_cervantes@hotmail.com>
2025-05-26 23:07:25 -0300

    refactor: simplify CreateValidOrder method by removing unused parameter

--- tests/TechFood.Doman.Tests/Fixtures/OrderFixture.cs
+++ tests/TechFood.Doman.Tests/Fixtures/OrderFixture.cs
@@ -12,9 +12,7 @@ namespace TechFood.Doman.Tests.Fixtures
             _faker = new Faker("pt_BR");
         }
 
-        public Order CreateValidOrder(Guid? customerId = null, int? number = null) =>
-            new(
-                number ?? _faker.Random.Int(1, 1000),
-                customerId);
+        public Order CreateValidOrder(Guid? customerId = null) =>
+            new(customerId);
     }
 }
